8e9b01eca17d9a494355b200eef2fac845cc9487
c-schutz <cschutz@ufl.edu>
2025-09-21 14:37:59 -0400

    fixed some things

--- server/index.js
+++ server/index.js
@@ -5,22 +5,27 @@ const dotenv = require('dotenv')
 dotenv.config()
 const mongoose = require('mongoose');
 
-const mongoPassword = process.env.DB_PASS;
-
-const connection = `mongodb+srv://cschutz_db_user:${mongoPassword}@clusterhackathon.tuxpv3d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterHackathon`;
-
-// Allow only the frontend dev server (adjust as needed)
+// Allow only the frontend dev server using CORS
 const corsOptions = {
   origin: 'http://localhost:5173', // Vite dev server
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 };
-
 app.use(cors(corsOptions));
 
-mongoose.connect("mongodb+srv://cschutz_db_user:5KWchNPBU69VBzkw@clusterhackathon.tuxpv3d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterHackathon")
-  .then(() => console.log('Connected!'));
+let connection;
+//use mongoose to connect to DB
+try{
+  const mongoPassword = process.env.DB_PASS;
+  connection = `mongodb+srv://cschutz_db_user:${mongoPassword}@clusterhackathon.tuxpv3d.mongodb.net/?retryWrites=true&w=majority&appName=ClusterHackathon`;
+}catch (error) {
+  console.log(".env not set properly");
+}
+
+mongoose.connect(connection) //make sure to add new ip address every time you want to run
+  .then(() => console.log('Connected!'))
+  .catch(() => console.log("Not Connected"));
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
